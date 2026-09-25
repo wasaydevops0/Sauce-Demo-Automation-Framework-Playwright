a73ac86052f7a5722bcb6bdf148a4e4020ae2eff
Abdul Wasay <106981654+wasaydevops0@users.noreply.github.com>
2026-09-26 00:29:04 +0500

    Add new test for 'get started' link visibility

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -14,3 +14,11 @@ test('get started link', async ({ page }) => {
 
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('get started link2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
